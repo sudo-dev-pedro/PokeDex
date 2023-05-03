20623201201a26a654c067c20d5d30d7fb189421
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2023-05-03 20:48:56 -0300

    [PKD-027] Adjust adapter

--- app/src/main/java/com/example/pokedex/adapter/EvolutionAdapter.kt
+++ app/src/main/java/com/example/pokedex/adapter/EvolutionAdapter.kt
@@ -3,20 +3,20 @@ package com.example.pokedex.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedex.R
 import com.example.pokedex.model.PokemonEvolutions
 
-class EvolutionAdapter (
-    private val pokemonEvolutions : List<PokemonEvolutions>
+class EvolutionAdapter(
+    private val pokemonEvolutions: List<PokemonEvolutions>
 ) : RecyclerView.Adapter<EvolutionAdapter.EvolutionViewHolder>() {
 
-    class EvolutionViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        val nomePrimeiraEvolucao : TextView = itemView.findViewById(R.id.txtViewForm1Name)
+    class EvolutionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val firstEvolution: TextView = itemView.findViewById(R.id.txtViewForm1Name)
+
         //val imagemPrimeiraEvolucao : ImageView = itemView.findViewById(R.id.imgViewForm1)
-        val nomeSegundaEvolucao : TextView = itemView.findViewById(R.id.txtViewForm2Name)
+        val secondEvolution: TextView = itemView.findViewById(R.id.txtViewForm2Name)
         //val imagemSegundaEvolucao : ImageView = itemView.findViewById(R.id.imgViewForm2)
     }
 
@@ -33,9 +33,9 @@ class EvolutionAdapter (
 
     override fun onBindViewHolder(holder: EvolutionAdapter.EvolutionViewHolder, position: Int) {
         holder.apply {
-            holder.nomePrimeiraEvolucao.text = pokemonEvolutions[position].nomeEvolucoes
+            holder.firstEvolution.text = pokemonEvolutions[position].evolutions
             //holder.imagemPrimeiraEvolucao.setImageResource(pokemonEvolutions[position].imagemPrimeiraEvolucao)
-            holder.nomeSegundaEvolucao.text = pokemonEvolutions[position].segundaEvolucao
+            holder.secondEvolution.text = pokemonEvolutions[position].secondEvolution
             //holder.imagemSegundaEvolucao.setImageResource(pokemonEvolutions[position].imagemSegundaEvolucao)
         }
     }
