d48dc0804b75c7fe91d68eb0277f2e73e6995d61
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2023-05-03 21:04:22 -0300

    [PKD-030] Adjust get image fun + Get the ID from the URL

--- app/src/main/java/com/example/pokedex/utils/PokemonUtils.kt
+++ app/src/main/java/com/example/pokedex/utils/PokemonUtils.kt
@@ -1,15 +1,21 @@
 package com.example.pokedex.utils
 
+import java.util.Locale
+
 class PokemonUtils {
 
     companion object {
-        fun capitalizeFirstLetterOfPokemon(string : String?) : String? = string?.capitalize()
+        fun capitalizeFirstLetterOfPokemon(string: String?): String? = string?.replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase(
+                Locale.ROOT
+            ) else it.toString()
+        }
 
-        fun makeIDMask(id : Int) : String {
+        fun makeIDMask(id: Int): String {
             val charBefore = "#"
             var masked = ""
 
-            masked = when(id){
+            masked = when (id) {
                 in 0..9 -> charBefore + "00" + "$id"
                 in 10..99 -> charBefore + "0" + "$id"
                 in 100..900 -> charBefore + "$id"
@@ -19,12 +25,12 @@ class PokemonUtils {
             return masked
         }
 
-        fun makeValuesMask(value : Int?) : String {
+        fun makeValuesMask(value: Int?): String {
             val charBefore = "0"
             var masked = ""
 
-            value?.let{
-                masked = when(value){
+            value?.let {
+                masked = when (value) {
                     in 0..99 -> charBefore + "$value"
                     else -> "$value"
                 }
@@ -32,9 +38,12 @@ class PokemonUtils {
             return masked
         }
 
-        fun prepareImages(id : Int) : String {
-            return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png"
+        fun getPokemonIDFromURL(url: String): Int {
+            return url.split("/")[6].toInt()
         }
 
+        fun prepareImages(id: Int): String {
+            return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"
+        }
     }
 }
