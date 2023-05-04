7abf4a0c55f6840d1a95de0371de2af5d1add19d
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2023-05-03 21:06:52 -0300

    [PKD-033] Adjust Pokemon class removing the Types

--- app/src/main/java/com/example/pokedex/model/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/model/Pokemon.kt
@@ -1,11 +1,6 @@
 package com.example.pokedex.model
 
-import me.sargunvohra.lib.pokekotlin.model.PokemonType
-import java.io.Serializable
-
 class Pokemon(
-        val id: Int,
-        val nome: String,
-        val tipoPrimario: String,
-        val tipos: List<PokemonType>
+    val name: String,
+    val url: String,
 )
