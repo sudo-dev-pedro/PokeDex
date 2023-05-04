f6fd7de1923fb282c525e99936ccb748cfda80f0
Pedro Henrique Rodrigues de Oliveira <pedronoveseis@gmail.com>
2023-05-03 21:03:41 -0300

    [PKD-029] Adjust pokemon adapter

--- app/src/main/java/com/example/pokedex/adapter/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokedex/adapter/PokemonAdapter.kt
@@ -1,6 +1,6 @@
 package com.example.pokedex.adapter
 
-import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,62 +11,77 @@ import com.bumptech.glide.Glide
 import com.example.pokedex.R
 import com.example.pokedex.`interface`.CellPokemonClickListener
 import com.example.pokedex.model.Pokemon
-import com.example.pokedex.model.PokemonType
 import com.example.pokedex.utils.PokemonUtils
 
 class PokemonAdapter(
-        private val pokemons : List<Pokemon>,
-        private val context : Context,
-        private val onCellClickListener : CellPokemonClickListener
-    ) : RecyclerView.Adapter<PokemonAdapter.PokemonViewHolder>() {
+    private val pokemonList: List<Pokemon>,
+    private val onCellClickListener: CellPokemonClickListener,
+) : RecyclerView.Adapter<PokemonAdapter.PokemonViewHolder>() {
 
-    class PokemonViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
-        val idPokemon : TextView = itemView.findViewById(R.id.txtPokemonID)
-        val nomePokemon : TextView = itemView.findViewById(R.id.txtPokemonName)
-        val imagemPokemon : ImageView = itemView.findViewById(R.id.imgPokemon)
-        val iconeTipoPrimario : ImageView = itemView.findViewById(R.id.txtViewIconPrimary)
-        var iconeTipoSecundario : ImageView = itemView.findViewById(R.id.txtViewIconSecondary)
+    class PokemonViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val pokemonId: TextView = itemView.findViewById(R.id.txtPokemonID)
+        val pokemonName: TextView = itemView.findViewById(R.id.txtPokemonName)
+        val pokemonImage: ImageView = itemView.findViewById(R.id.imgPokemon)
+        val iconeTipoPrimario: ImageView = itemView.findViewById(R.id.txtViewIconPrimary)
+        var iconeTipoSecundario: ImageView = itemView.findViewById(R.id.txtViewIconSecondary)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
         val item = LayoutInflater
-                .from(parent.context)
-                .inflate(R.layout.pokemon_item, parent, false)
+            .from(parent.context)
+            .inflate(R.layout.pokemon_item, parent, false)
 
         return PokemonViewHolder(item)
     }
 
     override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
-        holder.itemView.apply{
+        holder.itemView.apply {
             Glide
                 .with(holder.itemView.context)
-                .load(PokemonUtils.prepareImages(pokemons[position].id))
-                .into(holder.imagemPokemon)
-            holder.idPokemon.text = PokemonUtils.makeIDMask(pokemons[position].id)
-            holder.nomePokemon.text = PokemonUtils.capitalizeFirstLetterOfPokemon(
-                    pokemons[position].nome
-            )
-            holder.iconeTipoPrimario.setBackgroundResource(
-                    PokemonType.getPrimaryIconByType(
-                            pokemons[position].tipos.get(0).type.name
-                    )
-            )
-
-            if(pokemons[position].tipos.size == 2) {
-                holder.iconeTipoSecundario.setBackgroundResource(
-                        PokemonType.getPrimaryIconByType(
-                                pokemons[position].tipos.get(1).type.name
-                        )
+                .load(getPokemonImage(pokemonList[position].url))
+                .into(holder.pokemonImage)
+            holder.pokemonId.text =
+                PokemonUtils.makeIDMask(
+                    PokemonUtils.getPokemonIDFromURL(pokemonList[position].url)
                 )
-            }
+            holder.pokemonName.text = PokemonUtils.capitalizeFirstLetterOfPokemon(
+                pokemonList[position].name
+            )
+//            holder.iconeTipoPrimario.setBackgroundResource(
+//                    PokemonType.getPrimaryIconByType(
+//                            pokemons[position].tipos.get(0).type.name
+//                    )
+//            )
 
-            holder.itemView.setOnClickListener{
-                onCellClickListener.onPokemonCellClickListener(pokemons[position])
+//            if(pokemons[position].tipos.size == 2) {
+//                holder.iconeTipoSecundario.setBackgroundResource(
+//                        PokemonType.getPrimaryIconByType(
+//                                pokemons[position].tipos.get(1).type.name
+//                        )
+//                )
+//            }
+
+            holder.itemView.setOnClickListener {
+                onCellClickListener.onPokemonCellClickListener(pokemonList[position])
             }
         }
     }
 
     override fun getItemCount(): Int {
-        return pokemons.size
+        return pokemonList.size
+    }
+
+    private fun getPokemonImage(url: String): String {
+        Log.d("PEDRO", url)
+        val pokemonID = PokemonUtils.getPokemonIDFromURL(url)
+        Log.d("PEDRO", pokemonID.toString())
+        Log.d("PEDRO", "$BASE_URL_IMAGE${pokemonID}$IMAGE_EXTENSION")
+        return "$BASE_URL_IMAGE${pokemonID}$IMAGE_EXTENSION"
+    }
+
+    companion object {
+        const val BASE_URL_IMAGE =
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+        const val IMAGE_EXTENSION = ".png"
     }
 }
